d6c6e0da35f94fb5aad8a397c1d35c7a04cc1ca8
Pavan Savanur <pavan.savanur@tesco.com>
2021-04-22 23:00:35 +0530

    Added CurriesPAF

---
+++ src/main/scala/lectures/part2/CurriesPAF.scala
@@ -0,0 +1,99 @@
+package lectures.part2
+
+object CurriesPAF extends App {
+
+  // curried functions
+  val superAdder: Int => Int => Int =
+    x => y => x + y
+
+  val add3 = superAdder(3) // Int => Int => 3 + y
+  println(add3(5))
+  println(superAdder(3)(5))
+
+  // METHOD
+  def curriedAdder(x: Int)(y: Int): Int = x + y // curried method
+
+  val add4: Int => Int = curriedAdder(4) // lifting = ETA-EXPANSION
+  // val add4_1 = curriedAdder(4) // throws error
+
+  // functions != methods (JVM limitation)
+  def inc(x:Int): Int = x + 1
+  List(1,2,3).map(inc) // ETA-EXPANSION equals to List(1,2,3).map(x => inc(x))
+
+  // Partial function applications
+  val add5 = curriedAdder(5)(_) // Int => Int
+
+  // EXERCISE
+  val simpleAddFunction = (x: Int, y:Int) => x + y
+  def simpleAddMethod(x: Int, y: Int) = x + y
+  def curriedAddMethod(x: Int)(y: Int) = x + y
+
+  //add7: Int => Int = y => 7 + y
+  val add7 = (x: Int) => simpleAddFunction(7, x)
+  val add7_2 = simpleAddFunction.curried(7)
+
+  val add7_3 = curriedAddMethod(7) _  // PAR
+  val add7_4 = curriedAddMethod(7)(_) // PAR = alternate syntax
+
+  val add7_5 = simpleAddMethod(7, _: Int) // alternate syntax  for turning methods into function values
+               // y  => simpleAddMethod(7, y)
+  val add7_6 = simpleAddFunction(7, _: Int)
+
+  // underscores are powerful
+  def concatenator(a: String, b: String, c: String) = a + b + c
+  val insertName = concatenator("Hello, I'm ", _: String, ", how are you?") // x: String => concatenator(hello, x, how are you)
+  println(insertName("Pavan"))
+
+  val fillInTheBlanks = concatenator("Hello, ", _: String, _: String) // (x, y) => concatenator("Hello, ", x, y)
+  println(fillInTheBlanks("Pavan", "Scala is awesome!"))
+
+  // EXERCISES
+  /*
+    1.  Process a list of numbers and return their string representations with different formats
+        Use the %4.2f, %8.6f and %14.12f with a curried formatter function.
+   */
+  def curriedFormatter(s: String)(number: Double): String = s.format(number)
+  val numbers = List(Math.PI, Math.E, 1, 9.8, 1.3e-12)
+
+  val simpleFormat = curriedFormatter("%4.2f") _ // lift
+  val seriousFormat = curriedFormatter("%8.6f") _ // lift
+  val preciseFormat = curriedFormatter("%14.12f") _ // lift
+
+  println(numbers.map(simpleFormat))
+  println(numbers.map(seriousFormat))
+  println(numbers.map(preciseFormat))
+  println(numbers.map(curriedFormatter("%14.12f"))) // compiler does sweet eta-expansion for us
+
+  /*
+    2.  difference between
+        - functions vs methods
+        - parameters: by-name vs 0-lambda
+   */
+  def byName(n: => Int) = n + 1
+  def byFunction(f: () => Int) = f() + 1
+
+  def method: Int = 42
+  def parenMethod(): Int = 42
+  /*
+    calling byName and byFunction
+    - int
+    - method
+    - parenMethod
+    - lambda
+    - PAF
+   */
+  byName(23) // ok
+  byName(method) // ok
+  byName(parenMethod()) //ok
+  byName(parenMethod) //ok but beware ==> byName(parenMethod())
+  // byName(() => 42) // not ok
+  byName((() => 42)()) // ok
+  // byName(parenMethod _) // not ok
+
+  // byFunction(42) // not ok
+  // byFunction(method) // not ok!!!!! does not do ETA-expansion!
+  byFunction(parenMethod) // ok
+  byFunction(() => 42) // ok
+  byFunction(parenMethod _) // ok ===> byFunction(parenMethod)
+
+}
---
+++ src/main/scala/lectures/part2/LazyEvaluation.scala
@@ -0,0 +1,94 @@
+package lectures.part2
+
+object LazyEvaluation extends App {
+
+  // lazy DELAYS the evaluation of values
+  lazy val x: Int = throw new RuntimeException
+  // println(x)
+
+  lazy val y: Int = {
+    println("hello")
+    42
+  }
+  println(y)
+  println(y)
+
+  // examples of implications:
+  def sideEffectCondition: Boolean = {
+    println("Boo")
+    true
+  }
+  def simpleCondition: Boolean = false
+
+  lazy val lazyCondition = sideEffectCondition
+  println(if(simpleCondition && lazyCondition) "yes" else "no")
+
+  // in conjunction with call by name
+  def byNameMethod(n: => Int): Int = n + n + n + 1
+  def retrieveMagicValue = {
+    // side effect or a long computation
+    println("waiting")
+    Thread.sleep(1000)
+    42
+  }
+
+  println(byNameMethod(retrieveMagicValue)) // Thread.sleep is called thrice
+  // use lazy vals
+  def byNameMethodLazy(n: => Int) = {
+    lazy val t = n // only evaluated once
+    t + t + t + 1
+  }
+  println(byNameMethodLazy(retrieveMagicValue))
+
+  // filtering with lazy vals
+  def lessThan30(i: Int): Boolean = {
+    println(s"$i is less than 30?")
+    i < 30
+  }
+
+  def greaterThan20(i: Int): Boolean = {
+    println(s"$i is greater than 20?")
+    i > 20
+  }
+
+  val numbers = List(1, 25, 40, 5, 23)
+  val lt30 = numbers.filter(lessThan30)
+  val gt20 = lt30.filter(greaterThan20)
+  println(gt20)
+
+  val lt30Lazy = numbers.withFilter(lessThan30)
+  val gt20Lazy = lt30Lazy.withFilter(greaterThan20)
+  println()
+  println(gt20Lazy) // side effects are never applied, nothing is printed
+  gt20Lazy.foreach(println) // side effects are now printed, filters are applied one by one to each element on apply need basis
+
+  // for-comprehensions ise withFilter with guards
+  for {
+    a <- List(1, 2, 3) if a % 2 == 0 // use lazy vals!
+  } yield a + 1
+  List(1, 2, 3).withFilter(_ % 2 ==0).map(_ + 1)
+
+  /*
+  Exercise: implement a lazily evaluated, singly linked STREAM of elements.
+   */
+  abstract class MyStream[+A] {
+    def isEmpty: Boolean
+    def head: A
+    def tail: MyStream[A]
+
+    def #::[B >: A](element: B): MyStream[B] // prepend operator
+    def ++[B >: A](anotherStream: MyStream[B]): MyStream[B] // concatenate two streams
+
+    def foreach(f: A => Unit): Unit
+    def map[B](f: A => B): MyStream[B]
+    def flatMap[B](f: A => MyStream[B]): MyStream[B]
+    def filter(predicate: A => Boolean): MyStream[A]
+
+    def take(n: Int): MyStream[A] // takes the first n elements out of this stream
+    def takeAsList(n: Int): List[A]
+  }
+
+  object MyStream {
+    def from[A](start: A)(generator: A => A): MyStream[A] = ???
+  }
+}
